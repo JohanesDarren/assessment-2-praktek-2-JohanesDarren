4f51cb20b965407750f8b8d7964c185e99327f56
JohanesDarren <johanesdarrenyehuda@student.telkomuniversity.ac.id>
2024-12-12 10:25:31 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -2,26 +2,27 @@ import java.util.Scanner;
 
 public class Nomor1 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
-        public static void variabel (int N){
-            for (int i = 0; i <= N; i++) {
-                if (i % 3 == 0 && i % 5 == 0) {
-                    System.out.print("FizzBuzz");
-                } else if (i % 3 == 0) {
-                    System.out.print("Fizz");
-                } else if (i % 5 == 0) {
-                    System.out.print("Buzz");
-                } else{
-                    System.out.print("i");
-                }
-            }
-        }
+        variabel(N);
         /* Pekerjaan anda berakhir sini */
 
-        System.out.println(Nomor1.class.getDeclaredMethods().length); 
+        System.out.println(Nomor1.class.getDeclaredMethods().length);
     }
-    
+
+    public static void variabel(int N) {
+        for (int i = 0; i <= N; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FizzBuzz" + " ");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz" + " ");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz" + " ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
